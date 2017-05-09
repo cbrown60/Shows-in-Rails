134f5177963f3aa41b7fba7f7c297766ec804278
Chris <cbrown605@googlemail.com>
2017-05-09 08:18:33 +0100

    index working

--- app/controllers/show_controller.rb
+++
@@ -1,5 +0,0 @@
-class ShowController < ApplicationController 
-
-
-
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   scope path: 'api' do 
-    resources :animals 
+    resources :shows
   end
 
     # The priority is based upon order of creation: first created -> highest priority.
